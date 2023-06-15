fbbc09e2c2bcefb95462d4cd4ff3c48368376545
sweeti-11 <120358979+sweeti-11@users.noreply.github.com>
2023-06-15 13:10:59 +0530

    navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import logo from '../assest/Chaylogoo.png'
+import logo from '../assest/virtual_trading.png'
 import './Navbar.css' 
 import { NavLink,Link } from 'react-router-dom'
 const Navbar = () => {
@@ -17,7 +17,8 @@ const Navbar = () => {
        :"100px"}}
          >
             
-         </img></NavLink> 
+         </img>
+         </NavLink> 
       <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
