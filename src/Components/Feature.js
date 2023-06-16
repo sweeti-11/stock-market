cf5eac4f9e5d14362eb43e65f48b67df78a8e3a3
sweeti-11 <120358979+sweeti-11@users.noreply.github.com>
2023-06-16 13:14:32 +0530

    slider\

--- src/Components/Feature.js
+++ src/Components/Feature.js
@@ -176,7 +176,138 @@ const feature = () => {
                 </div>
             </div>
         </section>
+        <section class="section gray-bg bottom-animate">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-4 m-15px-tb">
+                        <a href="https://www.idealstock.in/process">
+                        <div class="p-45px-lr lg-p-5px-lr servies-home">
+                            <img class="max-width-120" src="https://www.idealstock.in/webasset/images/content_icon1.png" title="" alt=""/>
+                            <h6 class="m-10px-b">How To Subscribe</h6>
+                            <p class="m-0px" style={{color:"#454545"}}>We give complete support on your any query.</p>
+                        </div>
+                        </a>
+                    </div>
+                    <div class="col-md-4 m-15px-tb">
+                        <a href="https://www.idealstock.in/Service/index">
+                        <div class="p-45px-lr lg-p-5px-lr servies-home">
+                            <img class="max-width-120" src="https://www.idealstock.in/webasset/images/content_icon2.png" title="" alt=""/>
+                            <h6 class="m-10px-b">The Ideal Expertise</h6>
+                            <p class="m-0px" style={{color:"#454545"}}>Our practises to get your goal.</p>
+                        </div>
+                        </a>
+                    </div>
+                    <div class="col-md-4 m-15px-tb">
+                        <a href="https://www.idealstock.in/risk/riskindex">
+                        <div class="p-45px-lr lg-p-5px-lr servies-home">
+                            <img class="max-width-120" src="https://www.idealstock.in/webasset/images/content_icon3.png" title="" alt=""/>
+                            <h6 class="m-10px-b">Let's Start the Process</h6>
+                            <p class="m-0px" style={{color:"#454545"}}>Enroll yourself to get professional views.</p>
+                        </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <img src="https://www.idealstock.in/webasset/images/header-bg03.png" alt=""/>
+        </section>
+        <section class="section">
+            <div class="container">
+                <div class="process-step">
+                    <div class="row justify-content-between">
+                        <div class="col-lg-6 align-self-center m-15px-tb">
+                            <h3 class="h3 m-15px-b">TO KNOW ABOUT IDEAL STOCK </h3>
+                            <p class="m-0px font-4">Work with us the way you want.</p>
+                            <p class="m-0px font-2">Ideal Stock Investment Adviser is a registered Investment Advisor with Securities and Exchange Board of India (SEBI Registration No. INA000008747). Ideal Stock is india's leading investment advisor for retail and high net worth investors. our recommendations are based on a combination of extensive and in-depth fundamental and technical research of stocks.</p>
+                        </div>
+                        <div class="col-lg-5 m-15px-tb productsta-image">
+                            <div class="card overflow-hidden border-radius-15">
+                                <img src="https://www.idealstock.in/webasset/images/toknow.png" title="" alt=""/>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="section">
+            <div class="container">
+                <div class="row align-items-center justify-content-between">
+                    <div class="col-lg-6 text-center m-15px-tb productsta-image">
+                        <img class="imageborder" src="https://www.idealstock.in/webasset/images/productsta.png" title="" alt="" style={{width:"100%", height:"auto"}}/>
+                    </div>
+                    <div class="col-lg-5 m-15px-tb">
+                        <div class="h3 m-15px-b text-uppercase">Product Suitability</div>
+                        <p class="m-40px-b">The suitability of investment products will depend upon the risk tolerance of the investor. Ideal Stock provides investment advise based on the financial goal as well as on the basis on risk profiling. Investment product is based on the risk assessment which can be categorized as Low, Medium & High Risk.</p>
+                        <div class="text-center">
+                            {/* <img class="imagemeter" src="https://www.idealstock.in/webasset/images/metergif.gif" /> */}
+                            {/* <a href="https://www.idealstock.in/risk/riskindex">
+                                <h5 class="riskknow">Know Your Risk</h5>
+                            </a> */}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section id="services" class="services">
+      <div class="container">
 
+        <div class="section-title">
+          <h2>Services</h2>
+          <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>
+        </div>
+
+        <div class="row">
+          <div class="col-lg-4 col-md-6 d-flex align-items-stretch">
+            <div class="icon-box">
+              <div class="icon"><i class="bx bxl-dribbble"></i></div>
+              <h4><a href="">Lorem Ipsum</a></h4>
+              <p>Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi</p>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 mt-md-0">
+            <div class="icon-box">
+              <div class="icon"><i class="bx bx-file"></i></div>
+              <h4><a href="">Sed ut perspiciatis</a></h4>
+              <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore</p>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 mt-lg-0">
+            <div class="icon-box">
+              <div class="icon"><i class="bx bx-tachometer"></i></div>
+              <h4><a href="">Magni Dolores</a></h4>
+              <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia</p>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4">
+            <div class="icon-box">
+              <div class="icon"><i class="bx bx-world"></i></div>
+              <h4><a href="">Nemo Enim</a></h4>
+              <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis</p>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4">
+            <div class="icon-box">
+              <div class="icon"><i class="bx bx-slideshow"></i></div>
+              <h4><a href="">Dele cardo</a></h4>
+              <p>Quis consequatur saepe eligendi voluptatem consequatur dolor consequuntur</p>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4">
+            <div class="icon-box">
+              <div class="icon"><i class="bx bx-arch"></i></div>
+              <h4><a href="">Divera don</a></h4>
+              <p>Modi nostrum vel laborum. Porro fugit error sit minus sapiente sit aspernatur</p>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+    </section>
     </div>
   )
 }
