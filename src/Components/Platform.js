eb316f96c371ff6113a9cae4ed67080b574261f8
sweeti-11 <120358979+sweeti-11@users.noreply.github.com>
2023-06-20 14:55:41 +0530

    platform

--- src/Components/Platform.js
+++ src/Components/Platform.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import Footer from './Footer'
 import mobile from '../assest/mobile.png'
+import web from '../assest/web.png'
+
 import './platform.css'
 
 const Platform = () => {
@@ -23,6 +25,7 @@ const Platform = () => {
             </div>
             <section id="platform" className='platform'>
             <div className='container'>
+            <h2 className='text-center'>Mobile</h2>
             <div className='row'>
             <div className='col-lg-6'>
             <img src={mobile} style={{paddingTop:"76px", paddingLeft:"176px",paddingRight:"176px", paddingBottom:"76px"}}></img>
@@ -43,6 +46,33 @@ const Platform = () => {
             </div>
             <div className='col-lg-6'>
             <img src={mobile} style={{paddingTop:"76px", paddingLeft:"176px",paddingRight:"176px", paddingBottom:"76px"}}></img>
+            </div>
+            </div>
+            </div>
+            </section>
+            <section id="platform" className='platform'>
+            <div className='container'>
+            <h2 className='text-center'>Web</h2>
+            <div className='row'>
+            <div className='col-lg-6'>
+            <img src={web} className='img-fluid'></img>
+            </div>
+            <div className='col-lg-6'>
+            <h2>MULTIPLE WATCH LIST</h2>
+            <p>Now you can create multiple watch list and add scripts as per your preferences</p>
+            </div>
+            </div>
+            </div>
+            </section>
+            <section id="platform" className='platform'>
+            <div className='container'>
+            <div className='row'>
+            <div className='col-lg-6'>
+            <h2>MULTIPLE WATCH LIST</h2>
+            <p>Now you can create multiple watch list and add scripts as per your preferences</p>
+            </div>
+            <div className='col-lg-6'>
+            <img src={web} className='img-fluid'></img>
             </div>
             </div>
             </div>
--- src/Components/Service.js
+++ src/Components/Service.js
@@ -1,5 +1,10 @@
 import React from 'react'
 import Footer from './Footer'
+import './service.css'
+import { BsFillStarFill } from "react-icons/bs";
+import se from '../assest/serv.jpg'
+
+
 
 const Service = () => {
   return (
@@ -19,7 +24,119 @@ const Service = () => {
                     </div>
                 </div>
             </div>
+            <section id="services-1" class="services-1 sections-bg">
+      <div class="container aos-init aos-animate" data-aos="fade-up">
 
+        <div class="section-header">
+          <h2>Our Services</h2>
+          <p>Aperiam dolorum et et wuia molestias qui eveniet numquam nihil porro incidunt dolores placeat sunt id nobis omnis tiledo stran delop</p>
+        </div>
+
+        <div class="row gy-4 aos-init aos-animate" data-aos="fade-up" data-aos-delay="100">
+
+          <div class="col-lg-4 col-md-6">
+            <div class="service-item  position-relative">
+              <div class="icon">
+                {/* <i class="bi bi-activity"></i> */}
+                <BsFillStarFill className='fa-fade'/>
+              </div>
+              <h3>Nesciunt Mete</h3>
+              <p>Provident nihil minus qui consequatur non omnis maiores. Eos accusantium minus dolores iure perferendis tempore et consequatur.</p>
+              {/* <a href="#" class="readmore stretched-link">Read more <i class="bi bi-arrow-right"></i></a> */}
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6">
+            <div class="service-item position-relative">
+              <div class="icon">
+              <BsFillStarFill className='fa-fade'/>
+              </div>
+              <h3>Eosle Commodi</h3>
+              <p>Ut autem aut autem non a. Sint sint sit facilis nam iusto sint. Libero corrupti neque eum hic non ut nesciunt dolorem.</p>
+              {/* <a href="#" class="readmore stretched-link">Read more <i class="bi bi-arrow-right"></i></a> */}
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6">
+            <div class="service-item position-relative">
+              <div class="icon">
+              <BsFillStarFill className='fa-fade'/>
+              </div>
+              <h3>Ledo Markt</h3>
+              <p>Ut excepturi voluptatem nisi sed. Quidem fuga consequatur. Minus ea aut. Vel qui id voluptas adipisci eos earum corrupti.</p>
+              {/* <a href="#" class="readmore stretched-link">Read more <i class="bi bi-arrow-right"></i></a> */}
+            </div>
+          </div>
+          <div class="col-lg-4 col-md-6">
+            <div class="service-item position-relative">
+              <div class="icon">
+              <BsFillStarFill className='fa-fade'/>
+              </div>
+              <h3>Ledo Markt</h3>
+              <p>Ut excepturi voluptatem nisi sed. Quidem fuga consequatur. Minus ea aut. Vel qui id voluptas adipisci eos earum corrupti.</p>
+              {/* <a href="#" class="readmore stretched-link">Read more <i class="bi bi-arrow-right"></i></a> */}
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6">
+            <div class="service-item position-relative">
+              <div class="icon">
+              <BsFillStarFill className='fa-fade'/>
+              </div>
+              <h3>Asperiores Commodit</h3>
+              <p>Non et temporibus minus omnis sed dolor esse consequatur. Cupiditate sed error ea fuga sit provident adipisci neque.</p>
+              {/* <a href="#" class="readmore stretched-link">Read more <i class="bi bi-arrow-right"></i></a> */}
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6">
+            <div class="service-item position-relative">
+              <div class="icon">
+              <BsFillStarFill className='fa-fade'/>
+              </div>
+              <h3>Velit Doloremque</h3>
+              <p>Cumque et suscipit saepe. Est maiores autem enim facilis ut aut ipsam corporis aut. Sed animi at autem alias eius labore.</p>
+              {/* <a href="#" class="readmore stretched-link">Read more <i class="bi bi-arrow-right"></i></a> */}
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6">
+            <div class="service-item position-relative">
+              <div class="icon">
+              <BsFillStarFill className='fa-fade'/>
+              </div>
+              <h3>Dolori Architecto</h3>
+              <p>Hic molestias ea quibusdam eos. Fugiat enim doloremque aut neque non et debitis iure. Corrupti recusandae ducimus enim.</p>
+              {/* <a href="#" class="readmore stretched-link">Read more <i class="bi bi-arrow-right"></i></a> */}
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+    </section>
+    <section id="service-2" className='service-2'>
+    <div className='container'>
+    <div className='row'>
+        <div className='col-lg-6'>
+        <img src={se} alt="" className='img-fluid'></img>
+        </div>
+        <div class="col-lg-6 d-flex justify-content-center flex-column aos-init aos-animate" data-aos="fade-up">
+            <h3>Sunt consequatur ad ut est nulla</h3>
+            <p>Cupiditate placeat cupiditate placeat est ipsam culpa. Delectus quia minima quod. Sunt saepe odit aut quia voluptatem hic voluptas dolor doloremque.</p>
+            <ul>
+              <li><i class="bi bi-check"></i> <span>Ullamco laboris nisi ut aliquip ex ea commodo consequat.</span></li>
+              <li><i class="bi bi-check"></i><span> Duis aute irure dolor in reprehenderit in voluptate velit.</span></li>
+              <li><i class="bi bi-check"></i> <span>Facilis ut et voluptatem aperiam. Autem soluta ad fugiat</span>.</li>
+            </ul>
+            {/* <a href="#" class="btn btn-get-started align-self-start">Get Started</a> */}
+          </div>
+    </div>
+
+
+    </div>
+
+    </section>
     <Footer/>
     </div>
   )
