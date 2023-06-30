4bcebc34a73ebe2c89c3bac86f8c16143331b21d
sweeti-11 <120358979+sweeti-11@users.noreply.github.com>
2023-06-30 15:42:33 +0530

    platfo

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -112,10 +112,10 @@ const Navbar = () => {
             <NavLink className="nav-link" to="https://panel.bulleyetrade.com/">Signin</NavLink>
           </li> */}
           <li className="nav-item">
-            <NavLink className="nav-link" to="https://bulleyetrade.com/signup.php">Signup</NavLink>
+            <a className="nav-link" href="https://bulleyetrade.com/signup.php">Signup</a>
           </li>
           <li className="nav-item">
-            <NavLink className="nav-link" to="https://panel.bulleyetrade.com/login">Signin</NavLink>
+            <a className="nav-link" href="https://panel.bulleyetrade.com/login">Signin</a>
           </li>
             </ul>
           </div>
